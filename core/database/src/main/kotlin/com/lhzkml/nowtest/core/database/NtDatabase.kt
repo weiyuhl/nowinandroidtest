0b6125be1550f98d91487705f78aaa5d3bbd0219
weiyuhl <weiyuhl@users.noreply.github.com>
2026-05-01 18:31:44 +0800

    移除开发阶段数据库迁移记录

--- core/database/src/main/kotlin/com/lhzkml/nowtest/core/database/NtDatabase.kt
+++ core/database/src/main/kotlin/com/lhzkml/nowtest/core/database/NtDatabase.kt
@@ -1,6 +1,5 @@
 package com.lhzkml.nowtest.core.database
 
-import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
@@ -17,23 +16,7 @@ import com.lhzkml.nowtest.core.database.util.InstantConverter
         NewsResourceTopicCrossRef::class,
         TopicEntity::class,
     ],
-    version = 15,
-    autoMigrations = [
-        AutoMigration(from = 1, to = 2),
-        AutoMigration(from = 2, to = 3, spec = DatabaseMigrations.Schema2to3::class),
-        AutoMigration(from = 3, to = 4),
-        AutoMigration(from = 4, to = 5),
-        AutoMigration(from = 5, to = 6),
-        AutoMigration(from = 6, to = 7),
-        AutoMigration(from = 7, to = 8),
-        AutoMigration(from = 8, to = 9),
-        AutoMigration(from = 9, to = 10),
-        AutoMigration(from = 10, to = 11, spec = DatabaseMigrations.Schema10to11::class),
-        AutoMigration(from = 11, to = 12, spec = DatabaseMigrations.Schema11to12::class),
-        AutoMigration(from = 12, to = 13),
-        AutoMigration(from = 13, to = 14),
-        AutoMigration(from = 14, to = 15, spec = DatabaseMigrations.Schema14to15::class),
-    ],
+    version = 1,
     exportSchema = true,
 )
 @TypeConverters(
